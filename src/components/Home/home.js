40ebfd02996ecd924b4e74218ee0ffca31c503b7
Aidar06 <114663298+Aidar06@users.noreply.github.com>
2023-03-28 18:44:01 +0600

    homeAbout (#5)

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeHero from "./HomeHero/homeHero";
+import HomeAbout from "./HomeAbout/homeAbout";
 
 const Home = () => {
     return (
         <>
-         <HomeHero/>
+            <HomeHero/>
+            <HomeAbout/>
         </>
     );
 };
