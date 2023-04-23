753be5befa0c2712be0dd77a4a98120c79ccfee7
Aidar06 <kabylbekovajdar94@gmail.com>
2023-04-23 14:22:29 +0600

    fixAbout

--- src/pages/About/AboutUs/About.js
+++ src/pages/About/AboutUs/About.js
@@ -7,41 +7,24 @@ const About = () => {
     return (
         <section id="about">
             <h1>
-                <pre>Ишкердик Академиясы</pre>
+                Ишкердик Академиясы
             </h1>
             <div className="about">
                 <div className="about--text">
                     <h4>
-                        <pre>Академия Тууралуу</pre>
+                        Академия Тууралуу
                     </h4>
-                    <p><pre>
-                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br/>
-                           Accusantium ad autem deserunt et facere hic inventore, laborum, <br/>
-                           nihil numquam praesentium quae quas quibusdam reprehenderit sequi <br/>
-                           sint sit temporibus totam veniam!
-                       </pre>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi atque cupiditate
+                        dicta,
+                        enim illo incidunt laboriosam magnam minima necessitatibus, nulla officia reiciendis
+                        soluta,
+                        tenetur vero! Adipisci architecto cum earum eveniet illum iure maxime minima officia
+                        possimus
+                        praesentium. At consequatur dicta eius impedit iusto magni praesentium quas quo, quod
+                        totam.
                     </p>
-                    <h1>
-                        <pre>Ишкердик Академиясы</pre>
-                    </h1>
-                    <div className="about">
-                        <div className="about--text">
-                            <h4>
-                                <pre>Академия Тууралуу</pre>
-                            </h4>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi atque cupiditate
-                                dicta,
-                                enim illo incidunt laboriosam magnam minima necessitatibus, nulla officia reiciendis
-                                soluta,
-                                tenetur vero! Adipisci architecto cum earum eveniet illum iure maxime minima officia
-                                possimus
-                                praesentium. At consequatur dicta eius impedit iusto magni praesentium quas quo, quod
-                                totam.
-                            </p>
-                        </div>
-                        <img src={business} alt="img"/>
-                    </div>
                 </div>
+                <img src={business} alt="img"/>
             </div>
         </section>
     );
